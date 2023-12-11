a3e91b09b847c3e8616340cf27d52aab6805d363
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-11 17:22:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,29 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	let n=arr.length;
+  arr.sort(function(a,b){
+		return a-b;
+	});
+	let x=[];
+	let sum=0;
+  let sum1=0;
+	
+    for(let i=0;i<n-1;i++){
+      sum1=arr[0]+arr[1];
+    x[i]=sum1;
+    arr.splice(0,2,sum1);
+		arr.sort(function(a,b){
+		return a-b;
+	});
+}
+let res=x.map(function(e){
+  sum+=e;
+  return sum;
+});
+	}
   
+	return sum;
 }
 
 module.exports=mincost;
